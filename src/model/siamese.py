8697c70ce55ba0aa4a6491e9754f7f4530f85011
Struan Robertson <contact@struan.tech>
2025-07-30 13:58:25 +0100

    Fix model

--- src/model/siamese.py
+++ src/model/siamese.py
@@ -11,7 +11,7 @@ class SharedSiamese(nn.Module):
     def __init__(self, embedding_size=128):
         super().__init__()
         # Create ResNet-50 model without pretrained weights
-        self.model = torchvision.models.resnet18(weights=None)
+        self.model = torchvision.models.resnet50(weights=None)
 
         # Replace final FC layer with embedding layers
         self.model.conv1 = nn.Conv2d(
